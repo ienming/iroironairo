09e1b0c42f49409a8604fcedc67510692ac2910b
hsieh.alan.0726@gmail.com <hsieh.alan.0726@gmail.com>
2023-07-28 09:03:26 +0800

    Test

--- frontend/src/components/selectImg.vue
+++ frontend/src/components/selectImg.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div>
-        <p>累死了快發瘋</p>
+        <p>20230728 Friday</p>
         <!-- <p>now show: {{ props.imgNum }}</p> -->
         <button @click="$emit('prevImg')">Prev</button>
         <button @click="$emit('nextImg')">Next</button>
