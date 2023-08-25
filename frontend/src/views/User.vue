3b2086c1bf6d4d85a4c3f8caa911e6666874c988
ienming <hsieh.alan.0726@gmail.com>
2023-08-26 00:02:53 +0800

    fix bug while reading 'places' column from CSV

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import axios from 'axios'
 
 const data = ref([])
+const heroData = ref(undefined)
 
 // 
 const reorderColors = computed(()=>{
@@ -45,40 +46,61 @@ function readFromCSV() {
 
         let obj = {}
         for (let i=0; i<cols.length; i++){
-          if (cols[i] == 'colors' && parts[i]!==''){
+          if (cols[i] == 'colors' && parts[i]){
             let colorString = parts[i].replace(/'/g, '"')
             parts[i] = JSON.parse(colorString.slice(1, -1))
             obj['main_color'] = parts[i][0]
+          }
+          if (cols[i] == 'places' && parts[i]){
+            let placeString = parts[i].replace(/'/g, '"')
+            if (placeString.indexOf(',') !== -1){
+              parts[i] = JSON.parse(placeString.slice(1, -1))
+            }else{
+              parts[i] = JSON.parse(placeString)
+            }
           }
           obj[cols[i]] = parts[i]
         }
         data.value.push(obj)
       })
+      let randomNum = Math.floor(Math.random() * data.value.length)
+      heroData.value = data.value[randomNum]
       console.log("讀取本地 CSV 成功")
     })
     .catch((error) => {
       console.error(error);
     });
 }
+
+onMounted(() => {
+  readFromCSV()
+})
 </script>
 
 <template>
-  <div class="my-5 mx-md-5">
-    <h1 class="ff-serif fw-bolder">色々な色</h1>
-    <h2 class="mb-3 ff-serif">
-      iroironairo
-    </h2>
-    <button class="btn btn-outline-dark" @click="readFromCSV">私が見た日本を観る！</button>
+  <div class="my-5 mx-md-5 d-flex justify-content-between">
+    <div class="ff-serif">
+      <h1 class="fw-bolder">色々な色</h1>
+      <h2>iroironairo</h2>
+    </div>
+    <div v-if="heroData" class="placeholder">
+      <img :src="heroData.url_google" alt="" />
+      <p>{{ heroData.date }}</p>
+      <p>
+        <span v-for="place of heroData.places"
+        class="me-2">{{ place }}</span>
+      </p>
+    </div>
   </div>
   <main>
     <section class="d-flex flex-wrap">
       <transition-group name="fade">
         <div v-for="(d, idx) of reorderColors" :key="d.id"
+        class="d-flex flex-column justify-content-between"
         :style="{'transition-delay': idx*0.025+'s'}">
-          <!-- <p>{{ d.name }}</p>
-          <p>{{ d.colors }}</p> -->
           <div class="color-swatch"
           v-if="typeof(d.colors) == 'object'"
+          :data-description="d.description.slice(0,7)+'...'"
           :style="{ 'background-color': 'hsl(' + d.main_color.h + ',' + d.main_color.s + '%,' + d.main_color.l + '%)'}"></div>
         </div>
       </transition-group>
@@ -96,13 +118,32 @@ function readFromCSV() {
   max-height: var(--max-size);
   transition: all .4s cubic-bezier(0.165, 0.84, 0.44, 1);
   cursor: pointer;
+  position: relative;
+}
+
+.color-swatch::after{
+  content: attr(data-description);
+  background-color: #fff;
+  border-radius: 50px;
+  padding: 8px;
+  width: max-content;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform-origin: left 50%;
+  transform: scale(0);
+  transition: all .4s cubic-bezier(0.165, 0.84, 0.44, 1);
+  box-shadow: 0 1rem 3rem rgba(0,0,0,.175);
+  opacity: 0;
+  z-index: 1;
 }
 .color-swatch:hover{
   border-radius: 50%;
 }
 
-img {
-  max-width: 500px;
+.color-swatch:hover::after{
+  transform: scale(1);
+  opacity: 1;
 }
 
 .logo {
@@ -115,16 +156,6 @@ img {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
   }
 }
 </style>
