0195423cc40e7913c6368304db70373a7bbed128
ienming <hsieh.alan.0726@gmail.com>
2024-02-04 16:58:10 +0800

    update about and add link to behance, commit update cloud_storage.py at last time

--- frontend/src/views/user/About.vue
+++ frontend/src/views/user/About.vue
@@ -24,11 +24,27 @@ import Navigator from "@/components/Navigator.vue";
                     </div>
                 </section>
                 <section class="col-12 col-lg-6">
-                    <p>「色々」有「很多、各式各樣」的意思，而「色々な色」則是指「各式各樣的色彩、色彩斑斕」。
+                    <p>
+                        「色々な色」這個計畫是將我在日本作為交換學生的半年間，所玩所見的照片整理起來的資料視覺化作品。
                     </p>
                     <p>
-                        「色々な色」是我在日本生活所見、隨手拍下的相片中所萃取出的顏色。它們代表了這段時間過於龐大、難以整理的瑣碎生活，也暗示我在什麼時間、去了哪裡時看見這些色彩，並組合成那段記憶。
+                        「色々」有「很多、各式各樣」的意思，而「色々な色」則是指「各式各樣的色彩、色彩斑斕」。 起初這個詞是在神戶大學「中級會話」課程的期中考中，自我介紹時無意間講出來的詞。因為發音很有趣、有某種回文的韻味，於是有了「以顏色為主題紀錄交換生活」的想法。
                     </p>
+                    <p>
+                        透過萃取照片中的顏色並整理後，它們呈現我在那段時間生活、旅遊、第一次在異國長時間居住時，所看見與自己背景文化的差異。也代表我關注、喜歡，且一直注視的那些重複的主題和色彩，並以此構成我的經驗與記憶。
+                    </p>
+                    <a class="txt-lang-hover link-dark link-offset-2"
+                    target="_blank"
+                    href="https://www.behance.net/gallery/186477055/-iroironairo">
+                        <div class="txt-lang-container">
+                            <span>專案過程
+                                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                            </span>
+                            <span>プロセス
+                                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                            </span>
+                        </div>
+                    </a>
                     <p class="text-end" style="font-size: 12px;">
                         Copyright © 2023 Lucien Hsieh
                     </p>
